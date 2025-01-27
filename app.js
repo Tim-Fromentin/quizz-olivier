c54df8875f7eebb39d490cb8a07419d35c981fff
Tim <tim.fromentin.dev.pro@gmail.com>
2025-01-27 14:50:59 +0100

    finish js

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
 const responses = document.querySelectorAll('.response')
+const boxResponses = document.querySelectorAll('.box_choice')
+const seeScore = document.getElementById('calculScore')
+const lenght = responses.length
+console.log(lenght)
+
+
 // console.log(response.value)
-responses.forEach(function (element){
-    element.addEventListener('click', test)
+
+
+
+    responses.forEach(function (element, index){
+
+
+
+        element.addEventListener('click', handleResponse)
+        element.addEventListener('click', () => {
+            if(element.classList.contains('r_1')) {
+                responses[index + 1].disabled = true
+            } else if (element.classList.contains('r_2')){
+                responses[index - 1].disabled = true
+            }
+        })
+
+
+
+        element.addEventListener('click', scoreCalcul)
+
 })
 
-function  test(element){
+
+
+let l = 0
+
+let i = 0
+
+
+function  handleResponse(element){
     console.log(element.target.value)
-    if(element.target.value === 'false'){
-        element.target.classList.add('response--false')
-    } else if (element.target.value === 'true'){
-        element.target.classList.add('response--true')
-    } else {
-        console.log('error no correct value')
+
+        if(element.target.value === 'false'){
+            element.target.classList.add('response--false')
+        l++
+        }
+        else if (element.target.value === 'true'){
+            element.target.classList.add('response--true')
+            l++
+        }
+        else {
+            console.log('error no correct value')
+        }
+        if (l === lenght / 2){
+            seeScore.classList.add('calculScore--active')
+        }
+
+
+
+}
+
+
+
+
+
+function  scoreCalcul(element){
+    console.log(element.target.value)
+    if(element.target.value === 'true'){
+        i++
+        console.log(i)
+    }
+    const score = document.getElementById('score')
+    seeScore.addEventListener('click', showScore)
+    function showScore(){
+        score.textContent = i
     }
 }
