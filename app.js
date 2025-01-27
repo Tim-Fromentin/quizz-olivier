e3e361630d0fc279e6974476e8e4482a732b519f
Tim <tim.fromentin.dev.pro@gmail.com>
2025-01-27 15:03:53 +0100

    purge js and reset btn

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// variable
 const responses = document.querySelectorAll('.response')
-const boxResponses = document.querySelectorAll('.box_choice')
+
 const seeScore = document.getElementById('calculScore')
+
 const lenght = responses.length
-console.log(lenght)
 
+let lenghtChoice = 0
 
-// console.log(response.value)
+let score = 0
+
 
 
 
@@ -14,13 +17,18 @@ console.log(lenght)
 
 
         element.addEventListener('click', handleResponse)
-        element.addEventListener('click', () => {
+        element.addEventListener('click', handleDisable)
+        function handleDisable(){
             if(element.classList.contains('r_1')) {
                 responses[index + 1].disabled = true
+                responses[index].disabled = true
+
             } else if (element.classList.contains('r_2')){
                 responses[index - 1].disabled = true
+                responses[index].disabled = true
+
             }
-        })
+        }
 
 
 
@@ -30,9 +38,7 @@ console.log(lenght)
 
 
 
-let l = 0
 
-let i = 0
 
 
 function  handleResponse(element){
@@ -40,16 +46,16 @@ function  handleResponse(element){
 
         if(element.target.value === 'false'){
             element.target.classList.add('response--false')
-        l++
+            lenghtChoice++
         }
         else if (element.target.value === 'true'){
             element.target.classList.add('response--true')
-            l++
+            lenghtChoice++
         }
         else {
             console.log('error no correct value')
         }
-        if (l === lenght / 2){
+        if (lenghtChoice === lenght / 2){
             seeScore.classList.add('calculScore--active')
         }
 
@@ -59,17 +65,21 @@ function  handleResponse(element){
 
 
 
-
-
 function  scoreCalcul(element){
     console.log(element.target.value)
     if(element.target.value === 'true'){
-        i++
-        console.log(i)
+        score++
+        console.log(score)
     }
-    const score = document.getElementById('score')
+    const scoreSum = document.getElementById('score')
     seeScore.addEventListener('click', showScore)
     function showScore(){
-        score.textContent = i
+        scoreSum.textContent = score
     }
+}
+
+const resetBtn = document.getElementById('resetBtn')
+resetBtn.addEventListener('click', resetQuizz)
+function resetQuizz(){
+    window.location.reload();
 }
